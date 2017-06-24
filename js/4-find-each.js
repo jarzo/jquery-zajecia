6a0202ee39ee49e6c6ba07425e78a3f5d68aad6a
Michał Jarząbek <nebraska994@gmail.com>
2017-06-24 15:27:50 +0200

    dodanie klasy paragraf-(numer indexu)

--- js/4-find-each.js
+++ js/4-find-each.js
@@ -1,1 +1,21 @@
 'use strict';
+
+//Funkcja document.ready()- wersja skrócona
+// find -znajduje nam elementy w wybranej kolekcji
+
+$(function(){
+	
+	
+	//Wybieranie elementu z kolekcji za pomocą metody eq()- to samo co index w js
+	$("body").find("p").eq(1).css({"color":"green"})
+
+	
+	//Pętla each dodająca do paragrafów klasę z kolejnym numerem
+	$("p").each(function(index){
+		$(this).addClass("paragraf-" + index);
+		console.log(jQuery(this) );
+	})
+
+
+
+});
