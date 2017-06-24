54e1f7354c461d148677bcf57aae8030d387a722
Michał Jarząbek <nebraska994@gmail.com>
2017-06-24 16:21:55 +0200

    finish

--- js/6-animacje.js
+++ js/6-animacje.js
@@ -2,4 +2,25 @@
 
 $(function() {
 	
+	//Efekt hide i show
+	$("p").hide(3000).show(3000);
+	
+	//Efekt fadeOut i fadeIn
+	$("h1").fadeOut(3000).fadeIn(3000);
+	
+	//Efekt slideUp i slideDown
+	$("h2").slideUp(3000).slideDown(3000);
+	
+	
+	//Funkcja zwrotna animacji- tzw.callback
+	function animacja () {
+		console.log("zakończono pierwszą animację. Zaraz startuje druga :)");
+	
+		$("h3").animate({'font-size':"1em","margin-left":"0px"},2000)
+		
+	}
+	
+	//Efekt animacji z funkcją zwrotną
+	$("h3").animate({'font-size': "3em", 'margin-left': "100px"},2000,animacja);
+	
 });
