880e4e0de7894dc0bf06ce635d9acc8b7cfb8662
Michał Jarząbek <nebraska994@gmail.com>
2017-06-24 14:30:24 +0200

    jak używać jQuery i jak znajdować elementy po...
    jak używać jQuery i jak znajdować elementy po...

---
+++ js/document-ready.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//korzystamy z metody document.ready(). żeby sprawdzić czy strona jest całkowicie załadowana
+
+//Funkcja document.ready() - wersja skrócona
+$(function() {
+  
+  //alert('test)
+	$('#first').css({color:'red'}).hide(2000).show(3000);
+  
+  });
+  
---
+++ js/selektory.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+$(function() {
+//	pobieramy element po znaczniku
+	var paragraf = $('p');
+	console.log(paragraf);
+	
+//	pobieramy element po id
+	var paragrafJeden = $('#paragraf');
+	console.log(paragrafJeden);
+	
+//	pobieramy element po klasie
+	var paragrafDwa = $('.paragraf');
+	console.log(paragrafDwa);
+	
+	
+})
